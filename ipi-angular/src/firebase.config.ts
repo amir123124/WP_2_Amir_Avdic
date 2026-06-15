a22b24b4aa13dd768f06878df1e8dd51bda2f6e6
Amir Avdic <aavdic41@gmail.com>
2026-06-15 11:39:18 +0200

    Dodana firebase konfiguracija

---
+++ ipi-angular/src/firebase.config.ts
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA4XH-yyiA2FzaKH3I20NVfYDOqDM5rZ3Q",
+  authDomain: "wp2-ahmethadziaganovic.firebaseapp.com",
+  projectId: "wp2-ahmethadziaganovic",
+  storageBucket: "wp2-ahmethadziaganovic.firebasestorage.app",
+  messagingSenderId: "701139519916",
+  appId: "1:701139519916:web:1356812425b2618b679209",
+  measurementId: "G-SNFDGCM7L3"
+};
+export default firebaseConfig;
