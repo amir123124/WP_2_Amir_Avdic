c670b10576c12c383ebded227367e944c80e43b7
Amir Avdic <aavdic41@gmail.com>
2026-06-15 11:39:22 +0200

    Dodan main.ts

---
+++ ipi-angular/src/main.ts
@@ -0,0 +1,18 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+import { App } from './app/app';
+import { routes } from './app/app.routes';
+
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import firebaseConfig from './firebase.config';
+
+bootstrapApplication(App, {
+  providers: [
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
+  ]
+}).catch(err => console.error('Bootstrap error:', err));
